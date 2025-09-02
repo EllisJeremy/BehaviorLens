15650ef89385f78b28d3f6494cacb94bdf47cf69
Jeremy Ellis <jeremyellis173@gmail.com>
2025-09-02 16:42:38 -0400

    feat: navbar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,71 @@
+import Octicons from "@expo/vector-icons/Octicons";
+import * as Haptics from "expo-haptics";
 import { Tabs } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="StartTest" options={{ title: "Start Test" }} />
-      <Tabs.Screen name="CreateTest" options={{ title: "Create Test" }} />
-      <Tabs.Screen name="PastTests" options={{ title: "Past Tests" }} />
-      <Tabs.Screen name="Settings" options={{ title: "Settings" }} />
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "hsla(208, 100%, 60%, 1.00)",
+      }}
+    >
+      <Tabs.Screen
+        name="StartTest"
+        options={{
+          title: "Start Test",
+          tabBarIcon: ({ color }) => (
+            <Octicons name="play" size={24} color={color} />
+          ),
+        }}
+        listeners={{
+          tabPress: () => {
+            Haptics.selectionAsync();
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="CreateTest"
+        options={{
+          title: "Create Test",
+          tabBarIcon: ({ color }) => (
+            <Octicons name="pencil" size={24} color={color} />
+          ),
+        }}
+        listeners={{
+          tabPress: () => {
+            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="PastTests"
+        options={{
+          title: "Past Tests",
+          tabBarIcon: ({ color }) => (
+            <Octicons name="stack" size={24} color={color} />
+          ),
+        }}
+        listeners={{
+          tabPress: () => {
+            Haptics.selectionAsync();
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="Settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color }) => (
+            <Octicons name="gear" size={24} color={color} />
+          ),
+        }}
+        listeners={{
+          tabPress: () => {
+            Haptics.selectionAsync();
+          },
+        }}
+      />
       <Tabs.Screen name="index" options={{ href: null }} />
     </Tabs>
   );
