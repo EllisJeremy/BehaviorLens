0fabf5f3e930e5b5a5bc627d03e5e211bfe4062f
Jeremy Ellis <jeremyellis173@gmail.com>
2025-09-04 17:48:47 -0400

    feat: test tile component

--- app/StartTest.tsx
+++ app/StartTest.tsx
@@ -1,16 +1,106 @@
-import { Text, View } from "react-native";
+import Octicons from "@expo/vector-icons/Octicons";
+import { StyleSheet, View } from "react-native";
+import StartTestTile from "./components/startTest/StartTestTile";
 
-export default function Index() {
+export default function StartTest() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "rgba(255, 255, 255, 1)",
-      }}
-    >
-      <Text>text example</Text>
+    <View style={styles.container}>
+      <StartTestTile
+        backgroundColor="#E3F2FD"
+        color="#1976D2"
+        icon="stopwatch"
+        title="On-Task Behavior"
+        time={30}
+      />
+      <StartTestTile
+        backgroundColor="#F3E5F5"
+        color="#7B1FA2"
+        icon="note"
+        title="ABC Data Collection"
+        time={15}
+      />
+
+      <StartTestTile
+        backgroundColor="#FFF3E0"
+        color="#F57C00"
+        icon="graph"
+        title="Engagement Tracking"
+        time={20}
+      />
+
+      <StartTestTile
+        backgroundColor="#E8F5E9"
+        color="#388E3C"
+        icon="checklist"
+        title="Task Completion"
+        time={25}
+      />
+
+      <View style={styles.circleButton}>
+        <Octicons name="plus" size={20} color="#fff" />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 70,
+    flex: 1,
+    backgroundColor: "#fff",
+
+    padding: 20,
+  },
+  card: {
+    width: "100%",
+    paddingVertical: 24,
+    borderRadius: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    marginVertical: 10,
+    paddingHorizontal: 20,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginLeft: 16,
+    color: "#333",
+  },
+  time: {},
+  newTestButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    marginTop: 30,
+    paddingVertical: 14,
+    paddingHorizontal: 20,
+    borderRadius: 12,
+    backgroundColor: "#0080ff",
+    width: "80%",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 4,
+  },
+  circleButton: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: "#0080ff",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+});
---
+++ app/components/startTest/StartTestTile.tsx
@@ -0,0 +1,63 @@
+import Octicons from "@expo/vector-icons/Octicons";
+import {
+  ColorValue,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+type StartTestProps = {
+  backgroundColor: ColorValue;
+  color: string;
+  icon: keyof typeof Octicons.glyphMap;
+  title: string;
+  time: number;
+};
+
+export default function StartTestTile({
+  backgroundColor,
+  color,
+  icon,
+  title,
+  time,
+}: StartTestProps) {
+  return (
+    <TouchableOpacity style={[styles.card, { backgroundColor }]}>
+      <Octicons name={icon} size={32} color={color} />
+      <View>
+        <Text style={styles.text}>{title}</Text>
+        <Text style={styles.time}>{time} min</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    width: "100%",
+    paddingVertical: 24,
+    borderRadius: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    marginVertical: 10,
+    paddingHorizontal: 20,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginLeft: 16,
+    color: "#333",
+  },
+  time: {
+    fontSize: 14,
+    marginLeft: 16,
+    color: "#5c5c5cff",
+  },
+});
