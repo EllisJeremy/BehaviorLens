5108c7d62b948c7b308f80262f796ffcd100a03f
Jeremy Ellis <jeremyellis173@gmail.com>
2025-09-05 19:23:18 -0400

    chore: remove old styles

--- components/startTest/StartTestTile.tsx
+++ components/startTest/StartTestTile.tsx
@@ -43,11 +43,6 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
     marginVertical: 10,
     paddingHorizontal: 20,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 3,
   },
   text: {
     fontSize: 18,
