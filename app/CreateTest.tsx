46f53984a7cc45033b69aea52f78e67a520958aa
Jeremy Ellis <jeremyellis173@gmail.com>
2025-09-11 16:01:39 -0400

    feat: add data access

--- app/CreateTest.tsx
+++ app/CreateTest.tsx
@@ -1,6 +1,17 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function CreateTest() {
+  useEffect(() => {
+    const loadData = async () => {
+      const storedName = await AsyncStorage.getItem("tests");
+      if (storedName) {
+        // retrieve
+      }
+    };
+    loadData();
+  }, []);
   return (
     <View
       style={{
