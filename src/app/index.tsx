ceaef73efaae7fe27282ac99e9deb951a98e5be5
Jeremy Ellis <jeremyellis173@gmail.com>
2025-10-08 19:26:23 -0400

    fix: ref for index

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
-import StartTest from "./StartTest";
+import { Redirect } from "expo-router";
 
 export default function Index() {
-  return <StartTest />;
+  return <Redirect href="/StartTest" />;
 }
