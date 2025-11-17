d5450cbd7871d5512ad30e6b3bfa833531885a79
Jeremy Ellis <jeremyellis173@gmail.com>
2025-11-17 17:08:31 -0500

    feat: styles for student tile

--- src/components/students/StudentTile.tsx
+++ src/components/students/StudentTile.tsx
@@ -1,5 +1,6 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { StudentType } from "../../app/Students";
+import Octicons from "@expo/vector-icons/Octicons";
 
 export default function StudentTile({
   firstName,
@@ -7,37 +8,64 @@ export default function StudentTile({
   grade,
 }: StudentType) {
   return (
-    <Pressable>
-      <View>
+    <View style={styles.tile}>
+      <View style={styles.info}>
         <Text style={styles.text}>
           {firstName} {lastName}
         </Text>
-        <Text style={styles.time}>{grade}</Text>
+        <Text style={styles.grade}>Grade {grade}</Text>
       </View>
-    </Pressable>
+
+      <View style={styles.controls}>
+        <Pressable style={styles.iconButton}>
+          <Octicons name="pencil" size={20} color="white" />
+        </Pressable>
+
+        <Pressable style={[styles.iconButton, styles.deleteButton]}>
+          <Octicons name="trash" size={20} color="white" />
+        </Pressable>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  card: {
+  tile: {
     width: "100%",
-    paddingVertical: 24,
-    borderRadius: 16,
+    backgroundColor: "#f1f1f1",
     flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "flex-start",
-    marginVertical: 10,
-    paddingHorizontal: 20,
+    padding: 14,
+    borderRadius: 10,
+    marginVertical: 8,
+  },
+  info: {
+    flexShrink: 1,
   },
   text: {
     fontSize: 18,
     fontWeight: "600",
-    marginLeft: 16,
     color: "#333",
   },
-  time: {
+  grade: {
     fontSize: 14,
-    marginLeft: 16,
-    color: "#5c5c5cff",
+    color: "rgba(92, 92, 92, 1)",
+    marginTop: 2,
+  },
+  controls: {
+    flexDirection: "row",
+    gap: 10,
+  },
+  iconButton: {
+    backgroundColor: "rgba(45, 164, 255, 1)",
+    width: 40,
+    height: 40,
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  deleteButton: {
+    backgroundColor: "rgba(255, 52, 52, 1)",
   },
 });
