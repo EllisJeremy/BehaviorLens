3d31f02d02a4ca821a61fe802643bda7109fce72
Jeremy Ellis <jeremyellis173@gmail.com>
2025-11-19 19:43:42 -0500

    slideUpModal component

---
+++ src/components/universal/SlideUpModal.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { View, Text, Pressable, TextInput, StyleSheet } from "react-native";
+import Modal from "react-native-modal";
+import { useStudentsStore } from "../../state/useStudentsStore";
+
+export default function SlideUpModal() {
+  const { open, setOpen, students, addStudent } = useStudentsStore();
+
+  const clearState = () => {
+    setFirstName("");
+    setLastName("");
+    setGrade("");
+    setOpen(false);
+  };
+
+  const save = () => {
+    if (!firstName) return;
+
+    addStudent({ firstName, lastName, grade });
+    clearState();
+  };
+
+  return (
+    <Modal
+      isVisible={open}
+      onBackdropPress={() => setOpen(false)}
+      style={styles.modal}
+    >
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Pressable onPress={clearState}>
+            <Text style={styles.save}>Cancel</Text>
+          </Pressable>
+
+          <Text style={{ fontSize: 18 }}>Add Student</Text>
+          <Pressable onPress={tryAddStudent}>
+            <Text style={styles.cancel}> {"  Save"}</Text>
+          </Pressable>
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  modal: {
+    justifyContent: "flex-end",
+    margin: 0,
+  },
+  container: {
+    backgroundColor: "white",
+    padding: 20,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    height: "92%",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  save: {
+    color: "rgba(45, 164, 255, 1)",
+    fontSize: 16,
+  },
+  cancel: {
+    color: "rgba(45, 164, 255, 1)",
+    fontSize: 16,
+  },
+});
