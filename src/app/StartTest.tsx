64190ae1cc3ed5ed72107070ce8ceb8aa38b702a
Jeremy Ellis <jeremyellis173@gmail.com>
2025-11-25 17:05:56 -0500

    feat: start tests uses same ui as students

--- src/app/StartTest.tsx
+++ src/app/StartTest.tsx
@@ -1,50 +1,52 @@
+import React, { useEffect } from "react";
+import { FlatList, StyleSheet, View, Pressable, Text } from "react-native";
+import StudentTile from "../components/students/StudentTile";
+import AddStudentModal from "../components/students/AddStudentModal";
 import Octicons from "@expo/vector-icons/Octicons";
-import { Pressable, StyleSheet, View } from "react-native";
-import StartTestTile from "../components/startTest/StartTestTile";
+import { useStudentsStore } from "../state/useStudentsStore";
 
 export default function StartTest() {
+  const { loadStudents, students, setOpen, open } = useStudentsStore();
+
+  useEffect(() => {
+    loadStudents();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <StartTestTile
-        backgroundColor="#E3F2FD"
-        color="#1976D2"
-        icon="stopwatch"
-        title="On-Task Behavior"
-        time={30}
-      />
-      <StartTestTile
-        backgroundColor="#F3E5F5"
-        color="#7B1FA2"
-        icon="note"
-        title="ABC Data Collection"
-        time={15}
-      />
+      {Object.keys(students).length > 0 ? (
+        <FlatList
+          data={Object.values(students)}
+          keyExtractor={(_, i) => i.toString()}
+          renderItem={({ item }) => (
+            <StudentTile
+              uuid={item.uuid}
+              firstName={item.firstName}
+              lastName={item.lastName}
+              grade={item.grade}
+            />
+          )}
+          ListHeaderComponent={() => (
+            <View style={{ height: 1, backgroundColor: "#d6d6d6" }} />
+          )}
+          ItemSeparatorComponent={() => (
+            <View style={{ height: 1, backgroundColor: "#d6d6d6" }} />
+          )}
+          ListFooterComponent={() => (
+            <View style={{ height: 1, backgroundColor: "#d6d6d6" }} />
+          )}
+        />
+      ) : (
+        <View style={styles.placeHolder}>
+          <Text>Press + to add Students</Text>
+        </View>
+      )}
 
-      <StartTestTile
-        backgroundColor="#FFF3E0"
-        color="#F57C00"
-        icon="graph"
-        title="Engagement Tracking"
-        time={20}
-      />
-
-      <StartTestTile
-        backgroundColor="#E8F5E9"
-        color="#388E3C"
-        icon="checklist"
-        title="Task Completion"
-        time={25}
-      />
-
-      <Pressable
-        style={({ pressed }) => [
-          styles.circleButton,
-          ,
-          pressed && { transform: [{ scale: 0.95 }] },
-        ]}
-      >
-        <Octicons name="plus" size={20} color="#fff" />
+      <Pressable style={styles.add} onPress={() => setOpen(true)}>
+        <Octicons name="plus" size={20} color="white" />
       </Pressable>
+
+      <AddStudentModal />
     </View>
   );
 }
@@ -54,23 +56,19 @@ const styles = StyleSheet.create({
     paddingTop: 70,
     flex: 1,
     backgroundColor: "#fff",
-    padding: 20,
+    alignItems: "flex-end",
+    justifyContent: "space-between",
   },
-  text: {
-    fontSize: 18,
-    fontWeight: "600",
-    marginLeft: 16,
-    color: "#333",
+  placeHolder: {
+    margin: "auto",
   },
-  circleButton: {
-    width: 64,
-    height: 64,
-    borderRadius: 32,
-    backgroundColor: "#0080ff",
-    alignItems: "center",
+  add: {
+    backgroundColor: "rgba(12, 185, 0, 1)",
+    width: 90,
+    height: 40,
     justifyContent: "center",
-    marginTop: 30,
-    elevation: 5,
-    transitionDelay: "1s",
+    alignItems: "center",
+    borderRadius: 8,
+    margin: 20,
   },
 });
