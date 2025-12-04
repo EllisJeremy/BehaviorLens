217b016036b453d177c1956363b4914d0e7093b3
Jeremy Ellis <jeremyellis173@gmail.com>
2025-12-03 21:14:53 -0500

    fix: border only shows in the middle

--- src/components/students/AddStudentModal.tsx
+++ src/components/students/AddStudentModal.tsx
@@ -30,29 +30,31 @@ export default function AddStudentModal() {
   }
 
   function AddStudentForm() {
+    const fields = [
+      {
+        value: prevFirstName,
+        placeholder: "First Name",
+        onChange: setFirstName,
+      },
+      { value: prevLastName, placeholder: "Last Name", onChange: setLastName },
+      { value: prevGrade, placeholder: "Grade", onChange: setGrade },
+    ];
+
     return (
       <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) => setFirstName(text)}
-          defaultValue={prevFirstName}
-          placeholder="First Name"
-          placeholderTextColor="#A0A0A0"
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) => setLastName(text)}
-          defaultValue={prevLastName}
-          placeholder="Last Name"
-          placeholderTextColor="#A0A0A0"
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) => setGrade(text)}
-          defaultValue={prevGrade}
-          placeholder="Grade"
-          placeholderTextColor="#A0A0A0"
-        />
+        {fields.map((field, i) => (
+          <TextInput
+            key={i}
+            style={[
+              styles.input,
+              i < fields.length - 1 && styles.inputSeparator,
+            ]}
+            defaultValue={field.value}
+            onChangeText={field.onChange}
+            placeholder={field.placeholder}
+            placeholderTextColor="#A0A0A0"
+          />
+        ))}
       </View>
     );
   }
@@ -71,15 +73,17 @@ export default function AddStudentModal() {
 
 const styles = StyleSheet.create({
   inputContainer: {
-    backgroundColor: "rgba(240, 240, 240, 1)",
+    backgroundColor: "rgba(240,240,240,1)",
     borderRadius: 15,
     paddingLeft: 15,
     paddingRight: 15,
     marginTop: 20,
   },
   input: {
+    paddingVertical: 10,
+  },
+  inputSeparator: {
     borderBottomWidth: 1,
-    padding: 10,
-    borderColor: "rgba(197, 197, 197, 1)",
+    borderColor: "rgba(197,197,197,1)",
   },
 });
