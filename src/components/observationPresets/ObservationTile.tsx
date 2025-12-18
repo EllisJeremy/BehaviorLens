6b4bd329f68392663e7b83c469dd153cadf46311
Jeremy Ellis <jeremyellis173@gmail.com>
2025-12-17 22:30:09 -0500

    feat: obs tile

---
+++ src/components/observationPresets/ObservationTile.tsx
@@ -0,0 +1,103 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import Octicons from "@expo/vector-icons/Octicons";
+import {
+  useStudentsStore,
+  StudentType,
+} from "@/src/state/students/useStudentsStore";
+import { useStudentsModalStore } from "@/src/state/students/useStudentsModalStore";
+
+export default function StudentTile({
+  uuid,
+  firstName,
+  lastName,
+  grade,
+}: StudentType) {
+  const { removeStudent } = useStudentsStore();
+  const {
+    setFirstName,
+    setLastName,
+    setGrade,
+    setOpen,
+    setPrevUUID,
+    setPrevFirstName,
+    setPrevLastName,
+    setPrevGrade,
+  } = useStudentsModalStore();
+  return (
+    <View style={styles.tile}>
+      <View style={styles.info}>
+        <Text style={styles.text}>
+          {firstName} {lastName}
+        </Text>
+        <Text style={styles.grade}>Grade {grade}</Text>
+      </View>
+
+      <View style={styles.controls}>
+        <Pressable
+          style={styles.iconButton}
+          onPress={() => {
+            setPrevFirstName(firstName);
+            setFirstName(firstName);
+            setPrevLastName(lastName);
+            setLastName(lastName);
+            setPrevGrade(grade);
+            setGrade(grade);
+            setPrevUUID(uuid);
+            setOpen(true);
+          }}
+        >
+          <Octicons name="pencil" size={20} color="white" />
+        </Pressable>
+
+        <Pressable
+          style={[styles.iconButton, styles.deleteButton]}
+          onPress={() => {
+            removeStudent(uuid);
+          }}
+        >
+          <Octicons name="trash" size={20} color="white" />
+        </Pressable>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  tile: {
+    borderColor: "#d6d6d6ff",
+    width: "100%",
+    backgroundColor: "#ffffffff",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 20,
+  },
+  info: {
+    flexShrink: 1,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#333",
+  },
+  grade: {
+    fontSize: 14,
+    color: "rgba(92, 92, 92, 1)",
+    marginTop: 2,
+  },
+  controls: {
+    flexDirection: "row",
+    gap: 10,
+  },
+  iconButton: {
+    backgroundColor: "rgba(45, 164, 255, 1)",
+    width: 40,
+    height: 40,
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  deleteButton: {
+    backgroundColor: "rgba(255, 52, 52, 1)",
+  },
+});
