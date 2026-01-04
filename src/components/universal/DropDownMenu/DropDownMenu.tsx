8e3823eeabc66bcdca61c580744e32e8074a5a05
Jeremy Ellis <jeremyellis173@gmail.com>
2026-01-04 17:56:43 -0500

    feat: modal

--- src/components/universal/DropDownMenu/DropDownMenu.tsx
+++ src/components/universal/DropDownMenu/DropDownMenu.tsx
@@ -1,27 +1,57 @@
 import { View, Text, StyleSheet, Pressable } from "react-native";
 import Entypo from "@expo/vector-icons/Entypo";
 import { colors } from "@/src/utils/colors";
+import Popover from "react-native-popover-view";
+import { useState } from "react";
 
 export default function DropDownMenu({
   title,
   options,
-  prevOption,
   value,
   setValue,
 }: {
   title: string;
   options: string[];
-  prevOption: string;
   value: string;
-  setValue: (v: any) => void;
+  setValue: (v: string) => void;
 }) {
+  const [open, setOpen] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>example</Text>
-      <Pressable style={styles.button}>
-        <Text style={styles.buttonText}>Unassigned</Text>
-        <Entypo style={styles.icon} name="select-arrows" />
-      </Pressable>
+      <Text style={styles.text}>{title}</Text>
+
+      <Popover
+        isVisible={open}
+        onRequestClose={() => setOpen(false)}
+        backgroundStyle={{ backgroundColor: "transparent" }}
+        popoverStyle={{ marginHorizontal: -10, backgroundColor: "transparent" }}
+        arrowSize={{ width: 0, height: 0 }}
+        from={
+          <Pressable style={styles.button} onPress={() => setOpen(true)}>
+            <Text style={styles.buttonText}>{value}</Text>
+            <Entypo style={styles.icon} name="select-arrows" />
+          </Pressable>
+        }
+      >
+        <View style={styles.menu}>
+          {options.map((opt) => (
+            <Pressable
+              key={opt}
+              style={({ pressed }) => [
+                styles.menuItem,
+                pressed && styles.menuItemPressed,
+              ]}
+              onPress={() => {
+                setValue(opt);
+                setOpen(false);
+              }}
+            >
+              <Text>{opt}</Text>
+            </Pressable>
+          ))}
+        </View>
+      </Popover>
     </View>
   );
 }
@@ -48,5 +78,20 @@ const styles = StyleSheet.create({
   icon: {
     fontSize: 18,
     color: colors.blue,
+  },
+  menu: {
+    backgroundColor: colors.lightGray,
+    borderRadius: 8,
+    minWidth: 150,
+    maxWidth: 200,
+    gap: 1,
+  },
+  menuItem: {
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    backgroundColor: colors.lighterGray,
+  },
+  menuItemPressed: {
+    backgroundColor: colors.lightGray,
   },
 });
