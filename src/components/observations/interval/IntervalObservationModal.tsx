fc5b1ab23b59904158aef15a14adcbd8baf4dd0b
Jeremy Ellis <jeremyellis173@gmail.com>
2026-01-17 18:34:13 -0500

    feat: pause button

--- src/components/observations/interval/IntervalObservationModal.tsx
+++ src/components/observations/interval/IntervalObservationModal.tsx
@@ -1,16 +1,23 @@
-import { ScrollView, StyleSheet, View, Text, FlatList } from "react-native";
-import { useEffect } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  Pressable,
+  Animated,
+} from "react-native";
+import { useEffect, useRef } from "react";
 import SlideUpModal from "../../universal/SlideUpModal";
 import IntervalTile from "./IntervalTile";
 import { useIntervalObservationStore } from "@/src/state/observations/useIntervalObservationStore";
 import { IntervalObservationPreset } from "@/src/types/observations/observationTypes";
 import { useStartObservationModalStore } from "@/src/state/observations/useStartObservationModalStore";
 import { constants } from "@/src/utils/constants";
-import { IntervalObservation } from "@/src/types/observations/intervalTypes";
 import { colors } from "@/src/utils/styles";
 import { useSettingsStore } from "@/src/state/settings/useSettingsStore";
 import { useTimer } from "use-timer";
 import { timeFormatter } from "@/src/utils/timeFormatter";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function IntervalObservationModal({
   preset,
@@ -20,7 +27,6 @@ export default function IntervalObservationModal({
   const { open, clearForm, currentInterval, observations, nextInterval } =
     useIntervalObservationStore();
   const { settings } = useSettingsStore();
-
   const { clearForm: clearStartForm } = useStartObservationModalStore();
 
   const {
@@ -30,27 +36,35 @@ export default function IntervalObservationModal({
     offTaskList,
   } = preset;
 
-  const { time, start, pause, reset } = useTimer({
+  const { time, start, pause, reset, status } = useTimer({
     interval: 1000,
   });
 
+  const borderAnim = useRef(new Animated.Value(2)).current;
+
   useEffect(() => {
-    if (open) {
-      start();
-    }
+    if (open) start();
   }, [open]);
 
   useEffect(() => {
     if (time === 0) return;
-
     if (time % observationIntervalSeconds === 0) {
       nextInterval(numberOfObservations);
     }
   }, [time]);
 
+  useEffect(() => {
+    Animated.parallel([
+      Animated.timing(borderAnim, {
+        toValue: status === "RUNNING" ? 3 : 2,
+        duration: 180,
+        useNativeDriver: false,
+      }),
+    ]).start();
+  }, [status]);
+
   function exit() {
     clearForm();
-
     setTimeout(() => {
       clearStartForm();
     }, constants.modalDelay);
@@ -73,14 +87,38 @@ export default function IntervalObservationModal({
           )}
         />
 
-        <View
-          style={[styles.controller, { borderTopColor: settings.themeColor }]}
-        >
-          <Text>{currentInterval}</Text>
-          <Text>{timeFormatter(time)}</Text>
-          <Text>
-            {observationIntervalSeconds - (time % observationIntervalSeconds)}
-          </Text>
+        <View style={styles.controller}>
+          <View>
+            <Text>{currentInterval}</Text>
+            <Text>{timeFormatter(time)}</Text>
+            <Text>
+              {observationIntervalSeconds - (time % observationIntervalSeconds)}
+            </Text>
+          </View>
+
+          <Pressable
+            onPress={() => {
+              status === "RUNNING" ? pause() : start();
+            }}
+          >
+            <Animated.View
+              style={[
+                styles.pause,
+                {
+                  borderWidth: borderAnim,
+                  paddingLeft: status === "RUNNING" ? 0 : 4,
+
+                  borderColor: settings.themeColor,
+                },
+              ]}
+            >
+              <Ionicons
+                name={status === "RUNNING" ? "pause-outline" : "play-outline"}
+                size={40}
+                color={settings.themeColor}
+              />
+            </Animated.View>
+          </Pressable>
         </View>
       </View>
     );
@@ -109,9 +147,19 @@ const styles = StyleSheet.create({
   },
   controller: {
     borderTopWidth: 1,
-
+    borderTopColor: colors.gray,
     height: 100,
-
     marginBottom: 25,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 20,
+  },
+  pause: {
+    width: 70,
+    height: 70,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 35,
+    marginRight: 20,
   },
 });
