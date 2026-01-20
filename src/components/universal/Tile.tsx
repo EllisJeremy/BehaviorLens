0e1fd4a02a3bee497251ecab8eed291754a5bb8d
Jeremy Ellis <jeremyellis173@gmail.com>
2026-01-20 17:41:24 -0500

    feat: universal tile

---
+++ src/components/universal/Tile.tsx
@@ -0,0 +1,133 @@
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ActionSheetIOS,
+  Alert,
+  ImageSourcePropType,
+} from "react-native";
+import Octicons from "@expo/vector-icons/Octicons";
+import { useObservationPresetsModalStore } from "@/src/state/observationPresets/useObservationPresetsModalStore";
+import { colors, fontSizes } from "@/src/utils/styles";
+
+export default function Tile({
+  title,
+  subTitle,
+  object,
+  onEdit,
+  onRemove,
+  onPress,
+  iconSource,
+}: {
+  title: string;
+  subTitle: string;
+  object: any;
+  onEdit?: (object: any) => void;
+  onRemove: (object: any) => void;
+  onPress?: (object: any) => void;
+  iconSource: ImageSourcePropType;
+}) {
+  return (
+    <Pressable
+      style={styles.tile}
+      onPress={() => {
+        if (onPress) {
+          onPress(object);
+        }
+      }}
+    >
+      <View style={styles.iconAndInfo}>
+        <Image source={iconSource} style={styles.icon} />
+        <View style={styles.info}>
+          <Text style={styles.text} numberOfLines={1} ellipsizeMode="tail">
+            {title}
+          </Text>
+          <Text style={styles.subText}>{subTitle}</Text>
+        </View>
+      </View>
+
+      <View style={styles.controls}>
+        {onEdit && (
+          <Pressable
+            style={styles.iconButton}
+            onPress={() => {
+              onEdit(object);
+            }}
+          >
+            <Octicons name="pencil" size={20} color={colors.white} />
+          </Pressable>
+        )}
+
+        <Pressable
+          style={[styles.iconButton, styles.deleteButton]}
+          onPress={() => {
+            ActionSheetIOS.showActionSheetWithOptions(
+              {
+                options: ["Cancel", "Delete"],
+                destructiveButtonIndex: 1,
+                cancelButtonIndex: 0,
+              },
+              (index: number) => {
+                if (index === 1) onRemove(object);
+              },
+            );
+          }}
+        >
+          <Octicons name="trash" size={20} color={colors.white} />
+        </Pressable>
+      </View>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  tile: {
+    borderColor: "#d6d6d6ff",
+    width: "100%",
+    backgroundColor: "#ffffffff",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 20,
+    gap: 10,
+  },
+  iconAndInfo: {
+    flexDirection: "row",
+    gap: 15,
+    alignItems: "center",
+    flex: 1,
+  },
+  icon: {
+    height: 30,
+    width: 30,
+  },
+  info: {
+    flexShrink: 1,
+  },
+  text: {
+    fontSize: fontSizes.text,
+    fontWeight: "600",
+  },
+  subText: {
+    fontSize: fontSizes.subText,
+    color: colors.darkGray,
+    marginTop: 2,
+  },
+  controls: {
+    flexDirection: "row",
+    gap: 10,
+  },
+  iconButton: {
+    backgroundColor: colors.blue,
+    width: 40,
+    height: 40,
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  deleteButton: {
+    backgroundColor: colors.red,
+  },
+});
--- src/utils/observationPresets/typeToIcon.ts
+++ src/utils/observationPresets/typeToIcon.ts
@@ -1,8 +1,9 @@
 import { ObservationPresetEnum } from "@/src/types/observations/observationTypes";
 import stopwatch from "@/assets/stopwatch.png";
 import abcSquares from "@/assets/abcSquares.png";
+import { ImageSourcePropType } from "react-native";
 
-export const typeToIcon: Record<ObservationPresetEnum, any> = {
+export const typeToIcon: Record<ObservationPresetEnum, ImageSourcePropType> = {
   interval: stopwatch,
   abc: abcSquares,
 };
